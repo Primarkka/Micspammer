0335a695163cefa29a7030c105949e2978acc0f3
Primarkka <98833738+Primarkka@users.noreply.github.com>
2022-11-04 13:39:34 +0200

    Improved code readability
    Improved code readability to Python_StyleGuide.pdf standards, changed the textbox relief to RIDGE

--- Micspammer.py
+++ Micspammer.py
@@ -13,26 +13,28 @@ import os
 from pygame import mixer
 
 """Initialization"""
-os.chdir(os.path.dirname(os.path.abspath(__file__))) # Changes path to relative path from the .py file.
+os.chdir(os.path.dirname(os.path.abspath(__file__))) # path relative to file.
 pg.mixer.init() #Initialize PyGame
-pg.mixer.pre_init(44100, 16, 2, 4096)  #freq etc. settings
+pg.mixer.pre_init(44100, 16, 2, 4096)  #freq etc. settings for pygame mixer.
 root = tk.Tk() #Initiliaze Tkinter
 
-root.title('Videogame Soundboard') #Name of the window
-screen = tk.Frame(master=root, bg="Green", padx=5) #Background
+root.title("Videogame Soundboard") #Name of the window
+screen = tk.Frame(master = root, bg = "Green", padx = 5) #Background
 screen.pack() #Geometry manager for the widgets
 
 
 """Input Textbox"""
 
 inputstring_var = tk.StringVar() 
-entry = tk.Entry(master=screen, borderwidth=3, width=10, 
-textvariable = inputstring_var,  relief=SUNKEN)
-entry.grid(row=0, column=0, columnspan=3, ipady=2, pady=2)
+
+entry = tk.Entry(master = screen, borderwidth = 3, width = 25, 
+textvariable = inputstring_var,  relief = RIDGE)
+
+entry.grid(row = 0, column = 0, columnspan = 3, ipady = 2, pady = 2)
 
 """Button functions"""
 
-def your_song():
+def play_your_song():
     mixer.music.load((str(entry.get()))) #Read the input from the textbox 
     mixer.music.play()
 
@@ -58,27 +60,27 @@ def play_ron():
 
 """Buttons for the soundboard"""
 
-button_1 = tk.Button(master=screen, text='Play your own song', padx=45, 
-                    pady=5, width=3, command = your_song)
-button_1.grid(row=1, column=0, pady=2)
+button_1 = tk.Button(master = screen, text = "Play your own song", padx = 45, 
+                    pady = 5, width = 3, command = play_your_song)
+button_1.grid(row = 1, column = 0, pady = 2)
 
-button_2 = tk.Button(master=screen, text='CSGO', padx=15,
-                     pady=5, width=3, command = play_csgo)
-button_2.grid(row=1, column=1, pady=2)
+button_2 = tk.Button(master = screen, text = "CSGO", padx = 15,
+                     pady = 5, width = 3, command = play_csgo)
+button_2.grid(row=1, column = 1, pady = 2)
 
-button_3 = tk.Button(master=screen, text='HLL', padx=15,
-                     pady=5, width=3, command = play_hll)     
+button_3 = tk.Button(master = screen, text = "HLL", padx = 15,
+                     pady = 5, width = 3, command = play_hll)     
 button_3.grid(row=1, column=2, pady=2)
 
-button_4 = tk.Button(master=screen, text='STOP', padx=15,
-                     pady=5, width=3, command = stop)
-button_4.grid(row=2, column=0, pady=2)
+button_4 = tk.Button(master = screen, text = "STOP", padx = 15,
+                     pady = 5, width = 3, command = stop)
+button_4.grid(row=2, column = 0, pady = 2)
 
-button_5 = tk.Button(master=screen, text='DCS', padx=45,
-                     pady=5, width=3, command = play_dcs)
-button_5.grid(row=2, column=1, pady=2)
+button_5 = tk.Button(master = screen, text = "DCS", padx = 45,
+                     pady = 5, width = 3, command = play_dcs)
+button_5.grid(row=2, column = 1, pady = 2)
 
-button_6 = tk.Button(master=screen, text='RoN', padx=15,
-                     pady=5, width=3, command = play_ron)
-button_6.grid(row=2, column=2, pady=2)
+button_6 = tk.Button(master=screen, text= "RoN", padx = 15,
+                     pady = 5, width = 3, command = play_ron)
+button_6.grid(row = 2, column = 2, pady = 2)
 root.mainloop()
